a928b745fe9c369920755bb07171a57a2b1e094a
Wojciech Zarski <wojciech.zarski@gmail.com>
2018-10-26 13:00:00 +0200

    Init repository

---
+++ src/test/com/samsung/webstore/contollers/HelloControllerTest.java
@@ -0,0 +1,12 @@
+package com.samsung.webstore.contollers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloControllerTest {
+
+    @Test
+    public void index() {
+    }
+}
